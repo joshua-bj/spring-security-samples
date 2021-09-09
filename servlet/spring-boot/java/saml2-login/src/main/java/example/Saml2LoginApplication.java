78e1b94b2a121a50d8f4f18b786845deb9a4751f
Josh Cummings <josh.cummings@gmail.com>
2021-09-08 21:33:13 -0600

    SAML 2.0 Logout sample

--- servlet/spring-boot/java/saml2-login/src/main/java/example/Saml2LoginApplication.java
+++ servlet/spring-boot/java/saml2-login/src/main/java/example/Saml2LoginApplication.java
@@ -15,11 +15,59 @@
  */
 package example;
 
+import java.io.File;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+
+import org.opensaml.security.x509.X509Support;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.saml2.core.Saml2X509Credential;
+import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrations;
+import org.springframework.security.saml2.provider.service.registration.Saml2MessageBinding;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class Saml2LoginApplication {
+
+	@Value("${classpath:credentials/rp-private.key}")
+	RSAPrivateKey key;
+
+	@Value("${classpath:credentials/rp-certificate.crt}")
+	File file;
+
+	@Bean
+	SecurityFilterChain app(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests((authorize) -> authorize
+				.mvcMatchers("/error").permitAll()
+				.anyRequest().authenticated()
+			)
+			.saml2Login(Customizer.withDefaults())
+			.saml2Logout(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	@Bean
+	RelyingPartyRegistrationRepository registrations() throws Exception {
+		X509Certificate certificate = X509Support.decodeCertificate(this.file);
+		Saml2X509Credential signing = Saml2X509Credential.signing(this.key, certificate);
+		RelyingPartyRegistration registration = RelyingPartyRegistrations.fromMetadataLocation("https://simplesaml-for-spring-saml.apps.pcfone.io/saml2/idp/metadata.php")
+				.registrationId("one")
+				.signingX509Credentials((credentials) -> credentials.add(signing))
+				.singleLogoutServiceBinding(Saml2MessageBinding.REDIRECT)
+				.build();
+		return new InMemoryRelyingPartyRegistrationRepository(registration);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Saml2LoginApplication.class, args);
