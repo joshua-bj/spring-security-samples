c07739b78b72b6597682cc3ba7a0869f1a6e5fb8
Josh Cummings <josh.cummings@gmail.com>
2024-10-29 13:35:57 -0600

    Attempt

---
+++ servlet/spring-boot/java/saml2/etc/metadata/saml20-idp-hosted.php
@@ -0,0 +1,16 @@
+<?php
+$metadata['http://idp-two.127-0-0-1.nip.io'] = array(
+	'host' => 'idp-two.127-0-0-1.nip.io',
+	'privatekey' => 'server.pem',
+	'certificate' => 'server.crt',
+	'auth' => 'example-userpass'
+);
+
+$metadata['http://idp-one.127-0-0-1.nip.io'] = array(
+	'host' => '__DEFAULT__',
+	'privatekey' => 'server.pem',
+	'certificate' => 'server.crt',
+	'auth' => 'example-userpass'
+);
+
+?>
---
+++ servlet/spring-boot/java/saml2/etc/metadata/two-relyingparties.php
@@ -0,0 +1,12 @@
+<?php
+$metadata['http://sp.127-0-0-1.nip.io:8080/saml2/metadata'] = array(
+    'AssertionConsumerService' => 'http://localhost:8080/login/saml2/sso/springtwo2023',
+    'SingleLogoutService' => 'http://localhost:8080/logout/saml2/slo',
+    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+    'simplesaml.nameidattribute' => 'emailAddress',
+    'assertion.encryption' => FALSE,
+    'nameid.encryption' => FALSE,
+    'validate.authnrequest' => FALSE,
+    'redirect.sign' => TRUE,
+);
+?>
